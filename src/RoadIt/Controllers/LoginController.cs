94d171b1125f3a1aee60602f3ed8d2d72f7c6ba7
jellepauchet <jelle.pauchet@telenet.be>
2016-10-25 11:42:50 +0200

    extra button voor roadselection

--- src/RoadIt/Controllers/LoginController.cs
+++ src/RoadIt/Controllers/LoginController.cs
@@ -12,11 +12,33 @@ namespace RoadIt.Controllers
         //
         // GET: /Login/
 
+        RoadItEntities entities;
         public ActionResult Index()
         {
-            var entities = new RoadItEntities();
-            
+            entities = new RoadItEntities();
             return View();
+        }
+
+        [HttpPost]
+        public Boolean checkLogin(string email, string password)
+        {
+            
+            var UserList = new List<string>();
+            foreach (var item in entities.Users)
+            {
+                UserList.Add(item.Email.ToString());
+            }
+            for (var i = 0; i > UserList.Count;i++)
+            {
+                if(UserList[i] == email.ToString());
+                {
+                       
+                }
+            }
+            Session["email"] = email;
+            Session["password"] = password;
+            
+            return (true);
         }
     }
 }
